79365477f5409fc6a3ab97190e31c102034c308a
truaddict <480471+truaddict@users.noreply.github.com>
2020-06-29 16:17:32 +0300

    russian translation

--- scripts/playerJoin.js
+++ scripts/playerJoin.js
@@ -8,8 +8,8 @@ console.log("playerJoin.js loaded");
 onPlayerInitialized(player => {
 	let playerName = player.name
 	system.executeCommand(`execute @a[name="${playerName}"] ~ ~ ~ scoreboard players add @s old 0`,{});
-	system.executeCommand(`execute @a[name="${playerName}",scores={old=0}] ~ ~ ~ title @s title §aWelcome to\n§eInto§6CMD\n§b@s`,{});
-	system.executeCommand(`execute @a[name="${playerName}",scores={old=0}] ~ ~ ~ tellraw @a {"rawtext":[{"text":"§d${playerName} just joined IntoCMD for the first time!!!"}]}`,{});
-	system.executeCommand(`execute @a[name="${playerName}",scores={old=1}] ~ ~ ~ title @s title §aWelcome back\n§b@s`,{});
+	system.executeCommand(`execute @a[name="${playerName}",scores={old=0}] ~ ~ ~ title @s title §aДобро пожаловать на\n§9Cool§aMC§a,\n§b@s`,{});
+	system.executeCommand(`execute @a[name="${playerName}",scores={old=0}] ~ ~ ~ tellraw @a {"rawtext":[{"text":"§d${playerName} пришел к нам впервые!"}]}`,{});
+	system.executeCommand(`execute @a[name="${playerName}",scores={old=1}] ~ ~ ~ title @s title §aС возвращением,\n§b@s`,{});
 	system.executeCommand(`execute @a[name="${playerName}",scores={old=0}] ~ ~ ~ scoreboard players set @s old 1`,{});
 })
