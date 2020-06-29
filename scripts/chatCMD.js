df4c9801c6e6474ad603cd5240bb3ed09307a6d8
HongyiMC <39605206+HongyiMC@users.noreply.github.com>
2020-06-29 01:26:47 -0700

    add .info

--- scripts/chatCMD.js
+++ scripts/chatCMD.js
@@ -1,4 +1,8 @@
 import {
+    getPlayerByNAME
+} from "ez:player";
+
+import {
     onChat
 } from "ez:chat";
 
@@ -6,11 +10,19 @@ const system = server.registerSystem(0, 0);
 console.log("chatCMD.js loaded");
 
 let suicide = ['.suicide'];
+let info = ['.info'];
 
 onChat((cmdObject) => {
     try {
         if (cmdObject.content === suicide[0]) {
 			system.executeCommand(`kill "${cmdObject.sender}"`, () => {});
+        }
+		let player = getPlayerByNAME(cmdObject.sender);
+		let playerName = player.name;
+        let playerXuid = player.xuid;
+		let playerUuid = player.uuid;
+		if (cmdObject.content === info[0]) {
+			system.executeCommand(`tellraw "${playerName}" {"rawtext":[{"text":"§b---§e${playerName}§b---\n§eXUID: §6${playerXuid}\n§aUUID: §6${playerUuid}"}]}`, () => {});
         }
     } catch(err) {
         console.error(err);
