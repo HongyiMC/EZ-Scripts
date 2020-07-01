a8a7c0d29a38c57999d669eea22f21a59480a1c3
HongyiMC <39605206+HongyiMC@users.noreply.github.com>
2020-07-01 03:46:47 -0700

    fix excluded ore

--- scripts/antiXray.js
+++ scripts/antiXray.js
@@ -1,5 +1,4 @@
 const system = server.registerSystem(0, 0);
-console.log("antiXray.js loaded");
 
 system.listenForEvent("minecraft:player_destroyed_block", ({data: eventData}) => {
 		const {
@@ -8,21 +7,19 @@ system.listenForEvent("minecraft:player_destroyed_block", ({data: eventData}) =>
 			block_position: blockPosition
 		} = eventData
 		let entityName = system.getComponent(entity, "minecraft:nameable" ).data.name
-		/*
+		
 		if (blockName == "minecraft:iron_ore") {
 			system.executeCommand(`tellraw @a[tag=staff] {"rawtext":[{"text":"§3${entityName}§7 mined §f${blockName.replace("minecraft:","")}§7 at §6${Object.values(blockPosition).join(", ")}"}]}`, () => {})
 		}
 		if (blockName == "minecraft:gold_ore") {
 			system.executeCommand(`tellraw @a[tag=staff] {"rawtext":[{"text":"§3${entityName}§7 mined §f${blockName.replace("minecraft:","")}§7 at §6${Object.values(blockPosition).join(", ")}"}]}`, () => {})
 		}
-		*/
 		if (blockName == "minecraft:diamond_ore") {
 			system.executeCommand(`tellraw @a[tag=staff] {"rawtext":[{"text":"§3${entityName}§7 mined §f${blockName.replace("minecraft:","")}§7 at §6${Object.values(blockPosition).join(", ")}"}]}`, () => {})
 		}
 		if (blockName == "minecraft:lapis_ore") {
 			system.executeCommand(`tellraw @a[tag=staff] {"rawtext":[{"text":"§3${entityName}§7 mined §f${blockName.replace("minecraft:","")}§7 at §6${Object.values(blockPosition).join(", ")}"}]}`, () => {})
 		}
-		/*
 		if (blockName == "minecraft:redstone_ore") {
 			system.executeCommand(`tellraw @a[tag=staff] {"rawtext":[{"text":"§3${entityName}§7 mined §f${blockName.replace("minecraft:","")}§7 at §6${Object.values(blockPosition).join(", ")}"}]}`, () => {})
 		}
@@ -32,14 +29,12 @@ system.listenForEvent("minecraft:player_destroyed_block", ({data: eventData}) =>
 		if (blockName == "minecraft:coal_ore") {
 			system.executeCommand(`tellraw @a[tag=staff] {"rawtext":[{"text":"§3${entityName}§7 mined §f${blockName.replace("minecraft:","")}§7 at §6${Object.values(blockPosition).join(", ")}"}]}`, () => {})
 		}
-		*/
 		if (blockName == "minecraft:emerald_ore") {
 			system.executeCommand(`tellraw @a[tag=staff] {"rawtext":[{"text":"§3${entityName}§7 mined §f${blockName.replace("minecraft:","")}§7 at §6${Object.values(blockPosition).join(", ")}"}]}`, () => {})
 		}
-		/*
 		if (blockName == "minecraft:quartz_ore") {
 			system.executeCommand(`tellraw @a[tag=staff] {"rawtext":[{"text":"§3${entityName}§7 mined §f${blockName.replace("minecraft:","")}§7 at §6${Object.values(blockPosition).join(", ")}"}]}`, () => {})
 		}
-		*/
 	}
 )
+console.log("antiXray.js loaded");
