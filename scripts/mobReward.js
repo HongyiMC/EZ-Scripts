fd0e438c7d985ff2d28332d84694904288596be7
HongyiMC <39605206+HongyiMC@users.noreply.github.com>
2020-07-01 03:47:24 -0700

    Add player drop money system

--- scripts/mobReward.js
+++ scripts/mobReward.js
@@ -16,44 +16,71 @@ function randomReward(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+//customize the percentage of player losing money when get killed by another player
+let LootPercentage = 5
+
+//customize how much money should a mob drop (min, max)
+const mobs = {
+	"zombie" : randomReward(1,1),
+	"zombie_villager_v2" : randomReward(1,1),
+	"husk" : randomReward(1,1),
+	"skeleton" : randomReward(1,1),
+	"stray" : randomReward(1,1),
+	"creeper" : randomReward(1,1),
+	"spider" : randomReward(1,1),
+	"cave_spider" : randomReward(1,1),
+	"witch" : randomReward(2,3),
+	"phantom" : randomReward(1,2),
+	"blaze" : randomReward(2,2),
+	"ghast" : randomReward(3,3),
+	"ender_dragon" : randomReward(200,200),
+	"piglin" : randomReward(1,3),
+	"zoglin" : randomReward(2,5),
+	"player" : randomReward(1,1)
+};
+
+function percentage(num){
+	if (LootPercentage > 100) {
+		console.log("Please do not enter a percentage that is higher than 100")
+		let LootPercentage = 100
+		return (num/100)*LootPercentage;
+	}
+	else {
+		return (num/100)*LootPercentage;
+	}
+}
+
 system.listenForEvent("minecraft:entity_death", ({data: eventData}) => {
-	const { 
-		entity: deadEntity, 
-		killer 
+	const {
+		entity: deadEntity,
+		killer
 	} = eventData;
-	const killerName = system.getComponent(killer, "minecraft:nameable").data.name;
+	const kill = system.getComponent(killer, "minecraft:nameable").data.name;
 	const deadEntityIdentifier = deadEntity.__identifier__.replace("minecraft:", "");
-	const player = getPlayerByNAME(killerName);
-	//console.log(JSON.stringify(deadEntity));
-	
-	const mobs = {
-		"zombie" : randomReward(1,1),
-		"zombie_villager_v2" : randomReward(1,1),
-		"husk" : randomReward(1,1),
-		"skeleton" : randomReward(1,1),
-		"stray" : randomReward(1,1),
-		"creeper" : randomReward(1,1),
-		"spider" : randomReward(1,1),
-		"cave_spider" : randomReward(1,1),
-		"witch" : randomReward(2,3),
-		"phantom" : randomReward(1,2),
-		"blaze" : randomReward(2,2),
-		"ghast" : randomReward(3,3),
-		"ender_dragon" : randomReward(200,200),
-		"piglin" : randomReward(1,3),
-		"zoglin" : randomReward(2,5)
-	};
+	const killerName = getPlayerByNAME(kill);
 	
 	if (deadEntityIdentifier in mobs) {
-		let moneyOld = getBalance(player)
-		updateBalance(player, mobs[deadEntityIdentifier], "add");
-		system.executeCommand(`title "${player.name}" actionbar §fYou earned §e$${mobs[deadEntityIdentifier]} §ffor killing §6${deadEntity.__identifier__}\n§7Previous: §b${moneyOld} §7=> Now: §b${getBalance(player)}`, () => {});
-		//console.log("Player " + player.name + " got $" + mobs[deadEntityIdentifier] + " for killing " + deadEntity.__identifier__);
-		//console.log(moneyOld, getBalance(player))
+		if (deadEntity.__identifier__ === "minecraft:player") {
+			let moneyOld = getBalance(killerName)
+			const victim = system.getComponent(deadEntity, "minecraft:nameable").data.name;
+			const victimName = getPlayerByNAME(victim);
+			const victimMoney = getBalance(victimName);
+			const result = percentage(victimMoney);
+			const vitimLoot = Math.round(result);
+			const deductVitimLoot = vitimLoot * -1
+			updateBalance(killerName, vitimLoot, "add")
+			system.executeCommand(`title "${killerName.name}" actionbar §fYou earned §e$${vitimLoot} §a(${LootPercentage} Percent) §ffor killing §6${victimName.name}\n§7Previous: §b${moneyOld} §7=> Now: §b${getBalance(killerName)}`, () => {});
+			updateBalance(victimName, deductVitimLoot, "deduct")
+			system.executeCommand(`title "${victimName.name}" actionbar §fYou lost §e$${vitimLoot} §a(${LootPercentage} Percent) §ffor getting killed by §6${victimName.name}\n§7Previous: §b${moneyOld} §7=> Now: §b${getBalance(killerName)}`, () => {});
+		}
+		else {
+			let moneyOld = getBalance(killerName)
+			updateBalance(killerName, mobs[deadEntityIdentifier], "add");
+			system.executeCommand(`title "${killerName.name}" actionbar §fYou earned §e$${mobs[deadEntityIdentifier]} §ffor killing §6${deadEntity.__identifier__}\n§7Previous: §b${moneyOld} §7=> Now: §b${getBalance(killerName)}`, () => {});
+		}
 	}
 	else{
-		system.executeCommand(`title "${player.name}" actionbar §fYou killed §6${deadEntity.__identifier__}`, () => {});
-		//console.log("Other mobs detected");
+		system.executeCommand(`title "${killerName.name}" actionbar §fYou killed §6${deadEntity.__identifier__}`, () => {});
 	}
 })
 console.log("mobReward.js loaded");
