26e637a4edb761d94facb68307101d5cc0da0c43
HongyiMC <39605206+HongyiMC@users.noreply.github.com>
2020-07-03 22:45:34 -0700

    optimization and new nether ore

--- scripts/antiXray.js
+++ scripts/antiXray.js
@@ -8,31 +8,19 @@ system.listenForEvent("minecraft:player_destroyed_block", ({data: eventData}) =>
 		} = eventData
 		let entityName = system.getComponent(entity, "minecraft:nameable" ).data.name
 		
-		if (blockName == "minecraft:iron_ore") {
-			system.executeCommand(`tellraw @a[tag=staff] {"rawtext":[{"text":"§3${entityName}§7 mined §f${blockName.replace("minecraft:","")}§7 at §6${Object.values(blockPosition).join(", ")}"}]}`, () => {})
+		const mineBlocks = {
+			"minecraft:iron_ore" : blockName,
+			"minecraft:gold_ore": blockName,
+			"minecraft:diamond_ore": blockName,
+			"minecraft:lapis_ore": blockName,
+			"minecraft:redstone_ore": blockName,
+			"minecraft:lit_redstone_ore": blockName,
+			"minecraft:coal_ore": blockName,
+			"minecraft:emerald_ore": blockName,
+			"minecraft:quartz_ore": blockName,
+			"minecraft:nether_gold_ore": blockName
 		}
-		if (blockName == "minecraft:gold_ore") {
-			system.executeCommand(`tellraw @a[tag=staff] {"rawtext":[{"text":"§3${entityName}§7 mined §f${blockName.replace("minecraft:","")}§7 at §6${Object.values(blockPosition).join(", ")}"}]}`, () => {})
-		}
-		if (blockName == "minecraft:diamond_ore") {
-			system.executeCommand(`tellraw @a[tag=staff] {"rawtext":[{"text":"§3${entityName}§7 mined §f${blockName.replace("minecraft:","")}§7 at §6${Object.values(blockPosition).join(", ")}"}]}`, () => {})
-		}
-		if (blockName == "minecraft:lapis_ore") {
-			system.executeCommand(`tellraw @a[tag=staff] {"rawtext":[{"text":"§3${entityName}§7 mined §f${blockName.replace("minecraft:","")}§7 at §6${Object.values(blockPosition).join(", ")}"}]}`, () => {})
-		}
-		if (blockName == "minecraft:redstone_ore") {
-			system.executeCommand(`tellraw @a[tag=staff] {"rawtext":[{"text":"§3${entityName}§7 mined §f${blockName.replace("minecraft:","")}§7 at §6${Object.values(blockPosition).join(", ")}"}]}`, () => {})
-		}
-		if (blockName == "minecraft:lit_redstone_ore") {
-			system.executeCommand(`tellraw @a[tag=staff] {"rawtext":[{"text":"§3${entityName}§7 mined §f${blockName.replace("minecraft:","")}§7 at §6${Object.values(blockPosition).join(", ")}"}]}`, () => {})
-		}
-		if (blockName == "minecraft:coal_ore") {
-			system.executeCommand(`tellraw @a[tag=staff] {"rawtext":[{"text":"§3${entityName}§7 mined §f${blockName.replace("minecraft:","")}§7 at §6${Object.values(blockPosition).join(", ")}"}]}`, () => {})
-		}
-		if (blockName == "minecraft:emerald_ore") {
-			system.executeCommand(`tellraw @a[tag=staff] {"rawtext":[{"text":"§3${entityName}§7 mined §f${blockName.replace("minecraft:","")}§7 at §6${Object.values(blockPosition).join(", ")}"}]}`, () => {})
-		}
-		if (blockName == "minecraft:quartz_ore") {
+		if (blockName in mineBlocks) {
 			system.executeCommand(`tellraw @a[tag=staff] {"rawtext":[{"text":"§3${entityName}§7 mined §f${blockName.replace("minecraft:","")}§7 at §6${Object.values(blockPosition).join(", ")}"}]}`, () => {})
 		}
 	}
