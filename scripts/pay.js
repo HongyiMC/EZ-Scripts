2e2122406e7c2793f9a8f7e714c3dbf208c89c80
HongyiMC <39605206+HongyiMC@users.noreply.github.com>
2020-08-18 09:40:58 -0700

    Add pay command

---
+++ scripts/pay.js
@@ -0,0 +1,41 @@
+import {
+	getBalance,
+	updateBalance
+} from "ez:economy";
+
+import {
+	executeCommand,
+	registerCommand,
+	registerOverride
+} from "ez:command";
+
+import {
+	getPlayerByNAME
+} from "ez:player";
+
+//edit the percentage of tax here
+let taxPercentage = 5
+//edit the least amount of money a player can send
+let leastAmount = 20
+
+registerCommand("pay", "Transfer balance to other players.", 0);
+registerOverride("pay", [{type: "players", name: "player", optional: false}, {type: "int", name: "amount", optional: false}], function (targets, count) {
+	if (targets.length != 1) throw "You can only pay money to 1 player at a time.";
+	let sender = this.player
+	let target = targets[0];
+	let senderName = sender.name
+	let targetName = target.name
+	let finalAmount = Math.round((100 - taxPercentage) * count / 100)
+	if (this.player) {
+		if (count < leastAmount) {
+			throw "No Transactions under $" + leastAmount + " please.";
+		}else {
+			updateBalance(sender, -count, "deduct");
+			updateBalance(target, finalAmount, "add");
+			executeCommand("tellraw \"" + senderName + "\" {\"rawtext\":[{\"text\":\"You send " + targetName + " $" + count + "\ntax: " + taxPercentage + "% They get $" + finalAmount + "\"}]}");
+			executeCommand("tellraw \"" + targetName + "\" {\"rawtext\":[{\"text\":\"" + senderName + " send you $" + count + "\ntax: " + taxPercentage + "% you got $" + finalAmount + "\"}]}");
+		}
+	}
+});
+
+console.log("pay.js loaded");
