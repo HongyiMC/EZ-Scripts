eee04061104fc2da62d18f7e16b259d78cc5c88c
HongyiMC <39605206+HongyiMC@users.noreply.github.com>
2020-08-18 18:36:35 -0700

    Add customprefix source code

--- scripts/customprefix.js
+++ scripts/customprefix.js
@@ -1,1 +1,56 @@
-const _0x48ec=['customprefix','customprefix.js\x20loaded','Prefix','/customprefix','tellraw\x20\x22','input','§l§aCustom\x20§bPrefix\x20§eStore','\x20§r\x22','\x22\x20\x22§r§7~§r§l','player','insert\x20prefix\x20below','\x22\x20{\x22rawtext\x22:[{\x22text\x22:\x22§cno\x20can\x20do\x22}]}','change\x20prefix','custom-name\x20set\x20prefix\x20\x22','§6Price:\x20§e$','label','Bold','custom_form','error,\x20this\x20command\x20can\x20only\x20be\x20used\x20in\x20game!'];(function(_0x5d7656,_0x48ecb3){const _0xfeb822=function(_0x37bcb7){while(--_0x37bcb7){_0x5d7656['push'](_0x5d7656['shift']());}};_0xfeb822(++_0x48ecb3);}(_0x48ec,0x1ec));const _0xfeb8=function(_0x5d7656,_0x48ecb3){_0x5d7656=_0x5d7656-0x0;let _0xfeb822=_0x48ec[_0x5d7656];return _0xfeb822;};import{updateBalance}from'ez:economy';import{executeCommand,registerCommand,registerOverride}from'ez:command';import{send}from'ez:formui';registerCommand(_0xfeb8('0x2'),'Open\x20custom\x20prefix\x20store.',0x0),registerOverride(_0xfeb8('0x2'),[],function(){if(this[_0xfeb8('0xb')]){const _0x240526=0x1770;return send(this['player'],{'type':_0xfeb8('0x0'),'title':_0xfeb8('0x8'),'content':[{'type':_0xfeb8('0x11'),'text':_0xfeb8('0x10')+_0x240526},{'type':'toggle','text':_0xfeb8('0x12'),'default':![]},{'type':_0xfeb8('0x7'),'text':_0xfeb8('0xc'),'placeholder':_0xfeb8('0x4')}]},_0x9aa6f2=>{if(_0x9aa6f2==null)return;let _0x4c2c21=this[_0xfeb8('0xb')]['name'],[_0x3faae4,_0x5109b9,_0x44c936]=_0x9aa6f2;try{updateBalance(this[_0xfeb8('0xb')],-_0x240526,_0xfeb8('0xe'));if(_0x5109b9)executeCommand(_0xfeb8('0xf')+_0x4c2c21+_0xfeb8('0xa')+_0x44c936+_0xfeb8('0x9'));else executeCommand(_0xfeb8('0xf')+_0x4c2c21+'\x22\x20\x22§r§7~§r'+_0x44c936+_0xfeb8('0x9'));}catch{executeCommand(_0xfeb8('0x6')+_0x4c2c21+_0xfeb8('0xd'));}}),null;}throw[_0xfeb8('0x1'),_0xfeb8('0x5')];}),console['log'](_0xfeb8('0x3'));
+import {
+	updateBalance
+} from "ez:economy";
+
+import {
+	executeCommand,
+	registerCommand,
+	registerOverride
+} from "ez:command";
+
+import {
+	send
+} from "ez:formui";
+
+registerCommand("customprefix", "Open custom prefix store.", 0);
+registerOverride("customprefix", [], function () {
+	if (this.player) {
+		const price = 6000
+		send(this.player, {
+			type: "custom_form",
+			title: "§l§aCustom §bPrefix §eStore",
+			content: [
+				{
+					"type": "label",
+					"text": "§6Price: §e$" + price
+				},
+				{
+					"type": "toggle",
+					"text": "Bold",
+					"default": false
+				},
+				{
+					"type": "input",
+					"text": "insert prefix below",
+					"placeholder": "Prefix"
+				}
+			]
+		}, data => {
+			if (data == null) return;
+			let playerName = this.player.name
+			let [placeholder, bold, prefix] = data;
+			try {
+				updateBalance(this.player, -price, "change prefix");
+				if (bold) executeCommand("custom-name set prefix \"" + playerName + "\" \"§r§7~§r§l" + prefix + " §r\"");
+				else executeCommand("custom-name set prefix \"" + playerName + "\" \"§r§7~§r" + prefix + " §r\"");
+			} catch {
+				executeCommand("tellraw \"" + playerName + "\" {\"rawtext\":[{\"text\":\"§cno can do\"}]}");
+			}
+		}
+	);
+    return null
+  }
+  throw ["error, this command can only be used in game!", "/customprefix"]
+});
+
+console.log("customprefix.js loaded");
