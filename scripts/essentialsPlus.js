7715a77dbd94469823f26869bd77a7867b623800
HongyiMC <39605206+HongyiMC@users.noreply.github.com>
2020-09-29 23:41:13 -0700

    Add depth

--- scripts/essentialsPlus.js
+++ scripts/essentialsPlus.js
@@ -9,9 +9,14 @@ import {
 	registerOverride
 } from "ez:command";
 
+const system = server.registerSystem(0, 0);
+
+const seaLevel = 63
+
 registerCommand("suicide", "Commit suicide.", 0);
 registerCommand("info", "Show your connection info.", 0);
 registerCommand("balcheck", "Check other player's balance.", 0);
+registerCommand("depth", "Displays your current block depth.", 0);
 
 registerCommand("bc", "Broadcast a message.", 1);
 registerCommand("smite", "Smite players.", 1);
@@ -41,6 +46,19 @@ registerOverride("balcheck", [{type: "players", name: "player", optional: false}
 		if (targetName === thisName) executeCommand(`tellraw "${thisName}" {"rawtext":[{"text":"§6tips:\n§eyou can just use §3/balance §eto check your own balance"}]}`);
 	}else throw[targetName + "'s balance: " + targetBal];
 });
+registerOverride("depth", [], function () {
+	if (this.entity) {
+		let playerName = this.player.name;
+		let yPos = Math.floor(system.getComponent(this.entity.vanilla, "minecraft:position").data.y);
+		if (yPos > seaLevel) {
+			let yDifference = yPos - seaLevel
+			executeCommand(`tellraw "${playerName}" {"rawtext":[{"text":"§6You are §c${yDifference} §6block(s) above sea level."}]}`);
+		}else if (yPos < seaLevel) {
+			let yDifference = seaLevel - yPos
+			executeCommand(`tellraw "${playerName}" {"rawtext":[{"text":"§6You are §c${yDifference} §6block(s) below sea level."}]}`);
+		}else executeCommand(`tellraw "${playerName}" {"rawtext":[{"text":"§6You are at sea level."}]}`);
+	}else throw ["error, this command can only be used in game!"];
+})
 
 registerOverride("bc", [{type: "string", name: "text", optional: false}], function (content) {
 	executeCommand(`tellraw @a {"rawtext":[{"text":"${content}"}]}`);
