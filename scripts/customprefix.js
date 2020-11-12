aca539b39d635f6b99d0c04aa7712d8b00752b10
HongyiMC <39605206+HongyiMC@users.noreply.github.com>
2020-11-12 02:32:05 -0800

    update customprefix

--- scripts/customprefix.js
+++ scripts/customprefix.js
@@ -1,4 +1,5 @@
 import {
+	getBalance,
 	updateBalance
 } from "ez:economy";
 
@@ -12,10 +13,13 @@ import {
 	send
 } from "ez:formui";
 
+const price = 4000;
+const maxPrefixLength = 8;
+
 registerCommand("customprefix", "Open custom prefix store.", 0);
 registerOverride("customprefix", [], function () {
 	if (this.player) {
-		const price = 6000
+		const playerBal = getBalance(this.player)
 		send(this.player, {
 			type: "custom_form",
 			title: "§l§aCustom §bPrefix §eStore",
@@ -37,20 +41,21 @@ registerOverride("customprefix", [], function () {
 			]
 		}, data => {
 			if (data == null) return;
-			let playerName = this.player.name
+			let playerName = this.player.name;
 			let [placeholder, bold, prefix] = data;
-			try {
+			console.log(prefix.length);
+			if (playerBal < price) {
+				executeCommand(`tellraw "${playerName}" {"rawtext":[{"text":"§cno can do, you need $${price}"}]}`);
+			}else if (prefix.length <= maxPrefixLength) {
 				updateBalance(this.player, -price, "change prefix");
 				if (bold) executeCommand("custom-name set prefix \"" + playerName + "\" \"§r§7~§r§l" + prefix + " §r\"");
 				else executeCommand("custom-name set prefix \"" + playerName + "\" \"§r§7~§r" + prefix + " §r\"");
-			} catch {
-				executeCommand("tellraw \"" + playerName + "\" {\"rawtext\":[{\"text\":\"§cno can do\"}]}");
-			}
+			}else executeCommand(`tellraw "${playerName}" {"rawtext":[{"text":"§cno can do, prefix character limit: ${maxPrefixLength}"}]}`);
 		}
 	);
     return null
   }
-  throw ["error, this command can only be used in game!", "/customprefix"]
+  throw ["error, this command can only be used in game!"]
 });
 
 console.log("customprefix.js loaded");
